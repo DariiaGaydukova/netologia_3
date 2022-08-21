21daf3a6cd91c7f5363d7158dc0292e51f677248
Dariia Gaydukova <daria1171@gmail.com>
2022-08-21 17:40:34 +0300

    Version 2 (add readme)

--- netologiaGit1/src/Main.java
+++ netologiaGit1/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        int[] sales1 = {200, 300, 400};
+        SalesManager Ivanov = new SalesManager(sales1);
+        int max1 = Ivanov.max();
+
+        int[] sales2 = {1200, 200, 100};
+        SalesManager Petrov = new SalesManager(sales2);
+        int max2 = Petrov.max();
+
     }
-}
+
+    }
